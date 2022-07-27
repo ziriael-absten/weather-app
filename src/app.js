cf943dfa23abafd198e491d14e5002548f91748c
ziriael-absten <innli1322@gmail.com>
2022-07-27 18:56:45 +0300

    added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -30,6 +30,14 @@ function showTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    "http://openweathermap.org/img/wn/" +
+      response.data.weather[0].icon +
+      "@2x.png"
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let city = "New York";
 let apiKey = "c6b24904ab14ebc2fc4d82be19770cb4";
